0523020b2be421823651a984639d45ded0b7c5f4
themaxaboy <themaxaboy@hotmail.com>
2017-09-29 15:23:04 +0700

    charSet

--- renderer/components/Header.js
+++ renderer/components/Header.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default ({ title = "This is the default title" }) => (
   <Head>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <title>{title}</title>
 
     <link rel="stylesheet" href="/static/css/antd.min.css" />
