d315b552c8e44e2b9537451f3a871b7934cb9623
Veerapat Kumchom <vkumchom@dstworldwideservices.com>
2017-11-16 16:38:02 +0700

    del div

--- src/components/ActionMenu.js
+++ src/components/ActionMenu.js
@@ -12,34 +12,33 @@ export default class ActionMenu extends React.Component {
 
   render() {
     return (
-        <div>
-      <Collapse defaultActiveKey={["1"]}>
-        <Panel header="Action" key="1">
-          <p>
-            <a onClick={this.onBrowse}>
-              <Icon type="link" /> Import Logs Folder...
-            </a>
-          </p>
-          <p>
-            <a>
-              <Icon type="link" /> Export Logs...
-            </a>
-          </p>
-          <p>
-            <a>
-              <Icon type="link" /> Refresh
-            </a>
-          </p>
-          <p>
-            <a>
-              <Icon type="link" /> Help
-            </a>
-          </p>
-        </Panel>
-        <Panel header="Logs" key="2">
-          <p />
-        </Panel>
-      </Collapse></div>
+        <Collapse defaultActiveKey={["1"]}>
+          <Panel header="Action" key="1">
+            <p>
+              <a onClick={this.onBrowse}>
+                <Icon type="link" /> Import Logs Folder...
+              </a>
+            </p>
+            <p>
+              <a>
+                <Icon type="link" /> Export Logs...
+              </a>
+            </p>
+            <p>
+              <a>
+                <Icon type="link" /> Refresh
+              </a>
+            </p>
+            <p>
+              <a>
+                <Icon type="link" /> Help
+              </a>
+            </p>
+          </Panel>
+          <Panel header="Logs" key="2">
+            <p />
+          </Panel>
+        </Collapse>
     );
   }
 }
